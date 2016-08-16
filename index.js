00ffece969fa41f333d274c6e3132ba22f4ae55b
Kristofer Sommestad <kristofer.sommestad@springworks.se>
2016-08-16 07:06:02 +0200

    Revert "Add sinon-as-promised"

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ global.should = require('should').noConflict();
 global.should.extend();
 
 global.sinon = require('sinon');
-global.sinonAsPromised = require('sinon-as-promised');
 
 require('should-sinon');
 require('should-http');
