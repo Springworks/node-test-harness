26c5046c6614c6bf72d5f68affdbb01bd084bbce
David Brockman Smoliansky <david@springworks.se>
2016-08-24 19:19:20 +0200

    fix: no longer taint global namespace with plugin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ global.should = require('should').noConflict();
 global.should.extend();
 
 global.sinon = require('sinon');
-global.sinonAsPromised = require('sinon-as-promised');
 
 require('should-sinon');
 require('should-http');
+require('sinon-as-promised');
